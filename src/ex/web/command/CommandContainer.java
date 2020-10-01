862fe2634ab20ff88d6259211fdddbf75f430736
ctaxx <bykov.stanislav.p@gmail.com>
2020-10-01 22:20:12 +0300

    further development of CommandContainer, adding of commands

--- src/ex/web/command/CommandContainer.java
+++ src/ex/web/command/CommandContainer.java
@@ -11,12 +11,24 @@ public class CommandContainer {
 		commands.put("getActualExs", new GetActualExsCommand());
 		commands.put("getAllExs", new GetAllExsCommand());
 		
+		commands.put("bookATicket", new BookATicketCommand());
+		commands.put("cancelBooking", new CancelBookingCommand());
+		
 		commands.put("signUp", new SignUpCommand());
 		commands.put("signIn", new SignInCommand());
 		commands.put("logout", new LogoutCommand());
 		
 		//admin
+		commands.put("getAllUsers", new GetAllUsersCommand());
+		commands.put("getCurrentUsers", new GetCurrentUsers());
+		commands.put("cancelEx", new CancelExCommand());
+	}
+	
+	public static Command get(String commandName) {
+		if (commandName == null || !commands.containsKey(commandName)) {
+			return commands.get("noCommand"); 
+		}
 		
-		
+		return commands.get(commandName);
 	}
 }
---
+++ src/ex/web/command/GetCurrentUsers.java
@@ -0,0 +1,10 @@
+package ex.web.command;
+
+public class GetCurrentUsers implements Command{
+
+	@Override
+	public void execute() {
+		
+	}
+
+}
